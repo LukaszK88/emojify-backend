d79a03267bb8c61ef20751ed4a9d040c5951301d
Lukasz <lukasz.kowal@awin.com>
2019-01-09 13:19:32 +0000

    face landmarks

--- src/Infrastructure/FaceApi/FaceRecognition.ts
+++ src/Infrastructure/FaceApi/FaceRecognition.ts
@@ -4,21 +4,22 @@ const MODELS_PATH = './src/Infrastructure/FaceApi/Models';
 
 class FaceRecognition {
 
+
+  private getExpression() {
+
+  }
+
   async faceExpression() {
     await faceDetectionNet.loadFromDisk(MODELS_PATH);
     await faceapi.nets.faceExpressionNet.loadFromDisk(MODELS_PATH);
 
-    const img = await canvas.loadImage('./src/resources/images/face.jpg');
-    const results = await faceapi.detectAllFaces(img, faceDetectionOptions)
+    const img = await canvas.loadImage('https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-9/10868132_10152610968255913_3496260210928457827_n.jpg?_nc_cat=103&_nc_ht=scontent-lht6-1.xx&oh=8185a238e4c45c866d5451a16bd8ff21&oe=5C8F09CF');
+    const results = await faceapi.detectSingleFace(img, faceDetectionOptions)
       .withFaceExpressions();
 
-    const out = faceapi.createCanvasFromMedia(img) as any;
-    faceapi.drawDetection(out, results.map(res => res.detection), { withScore: false });
-    faceapi.drawFaceExpressions(out, results.map(({ detection, expressions }) => ({ position: detection.box, expressions })));
-
-    saveFile('faceExpressionRecognition.jpg', out.toBuffer('image/jpeg'));
-    console.log('done, saved results to out/faceExpressionRecognition.jpg')
-    return results;
+    return {
+      ...results,
+    }
   }
 
   async detectAllFaces() {
